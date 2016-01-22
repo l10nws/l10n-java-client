1101c396d79bf685dc1594cede02e18d033cc78e
Anton Mokshyn <Anton_Mokshyn@epam.com>
2016-01-22 18:01:58 +0200

    added validation

--- l10n-core/src/main/java/ws/l10n/core/ScheduledReloadableMessageBundleContext.java
+++ l10n-core/src/main/java/ws/l10n/core/ScheduledReloadableMessageBundleContext.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class ScheduledReloadableMessageBundleContext extends ReloadableMessageBundleContext {
 
+    private static final int ONE_MINUTE = 60 * 1000;
     private static final int ONE_HOUR = 60 * 60 * 1000;
 
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
@@ -23,6 +24,9 @@ public class ScheduledReloadableMessageBundleContext extends ReloadableMessageBu
     public ScheduledReloadableMessageBundleContext(L10nClient l10nClient, String bundleKey, String bundleVersion,
                                                    long period) {
         super(l10nClient, bundleKey, bundleVersion);
+        if(period < ONE_MINUTE) {
+            throw new IllegalArgumentException("scheduler period cannot be less that 1 minute");
+        }
         this.period = period;
     }
 
